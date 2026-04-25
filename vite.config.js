2cc3bc0dcf331fdf622d5a66783cfe1b937efcfe
Eric Njiraini <menjiraini@gmail.com>
2026-04-25 18:28:46 +0300

    updated

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// Change 'africa-sdg-scorecard' to match your actual GitHub repo name
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: '/africa-sdg-scorecard/',
+  // Change this to '/' so Vercel can find your files at the root
+  base: '/', 
 })
